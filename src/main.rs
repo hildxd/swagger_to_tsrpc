806c36f3329b8db54637b68f069b90e0f3cf967e
hildxd <hildxd@outlook.com>
2022-12-06 11:40:53 +0800

    chore: 新增types文件，把config移动到types

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod core;
 async fn main() -> Result<()> {
     let mut sp = Spinner::new(Spinners::Dots9, "Loading Config".into());
     let config =
-        core::config::SwaggerConfig::from_url("http://192.168.0.124:12001/api/system/v3/api-docs")
+        core::types::SwaggerConfig::from_url("http://192.168.0.124:12001/api/system/v3/api-docs")
             .await?;
     // let config = core::config::SwaggerConfig::from_test().await?;
     sp.stop();
